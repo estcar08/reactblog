07b42024ddd87388a8ec35dc963b119bf1dba4f5
estcar08 <Carlos.Estrella@theksquaregroup.com>
2021-11-19 13:52:10 -0600

    update Card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -12,17 +12,6 @@ const useStyles = makeStyles({
   root: {
     minWidth: 200,
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
 });
 
 OutlinedCard.propTypes = {
